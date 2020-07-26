283c03fb708a5dd08511c189f18e6dbf76a959be
asifmohd94 <59363614+asifmohd94@users.noreply.github.com>
2020-07-27 01:33:50 +0530

    Add files via upload

--- models/file.js
+++ models/file.js
@@ -1,1 +1,23 @@
+const mongoose = require('mongoose');
 
+
+const fileSchema = new mongoose.Schema({
+    fileName: {
+        type: String,
+        required: true
+    },
+    originalName: {
+        type: String,
+        required: true
+    },
+    path: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+
+fileSchema.statics.uploadPath = '/csv_uploads';
+
+
+module.exports = mongoose.model('File', fileSchema);
