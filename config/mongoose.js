c3f258b87b0f3c387ac4fa4879608128027201a1
asifmohd94 <59363614+asifmohd94@users.noreply.github.com>
2020-07-27 01:31:28 +0530

    Add files via upload

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,1 +1,19 @@
+const mongoose= require('mongoose');
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+
+mongoose.connect('mongodb://localhost/CSV');
+
+const db=mongoose.connection;
+
+ db.on('error', console.error.bind(console,"Error while connecting"));
+
+ db.once('open', ()=>{
+     console.log("Connected to the database");
+ });
+
+ module.exports=db;
